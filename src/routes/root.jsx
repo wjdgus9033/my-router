fafe165aaca18f537bd7f432fa9d0e0aef8ab767
kim <wjdgus9033@naver.com>
2025-07-03 11:33:28 +0900

    Submitting Forms onChange

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation, } from "react-router-dom";
+import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation, useSubmit,} from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 import { useEffect } from "react";
 
@@ -17,6 +17,7 @@ export async function action() {
 export default function Root() {
   const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+  const submit = useSubmit();
 
   useEffect(() => {
     document.getElementById("q").value = q;
@@ -35,6 +36,9 @@ export default function Root() {
               type="search"
               name="q"
               defaultValue={q}
+              onChange={(event) => {
+                submit(event.currentTarget.form);
+              }}
             />
             <div
               id="search-spinner"
