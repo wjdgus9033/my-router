3e948f94ad13c53b4931c55ff5b3aa7921a431d7
kim <wjdgus9033@naver.com>
2025-07-03 11:28:52 +0900

    GET Submissions with Client Side Routing

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,10 @@
 import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation, } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return { contacts };
 }
 
