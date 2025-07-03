315b8fab91d52f7d37d36a838270a8b893281030
kim <wjdgus9033@naver.com>
2025-07-03 12:40:35 +0900

    Mutations Without Navigation - 즐겨찾기 토글 버튼

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,5 +1,12 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { Form, useLoaderData, useFetcher, } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+}
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
@@ -8,7 +15,7 @@ export async function loader({ params }) {
 
 export default function Contact() {
   const { contact } = useLoaderData();
-
+  
   return (
     <div id="contact">
       <div>
@@ -73,8 +80,10 @@ export default function Contact() {
 
 function Favorite({ contact }) {
   const favorite = contact.favorite;
+  const fetcher = useFetcher();
+
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -86,6 +95,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
